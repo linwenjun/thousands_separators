83995fddc3bbf95329f16bfd472a3ed37b36993f
linwenjun <linwenjun.com>
2016-03-23 21:10:37 +0800

    second step

--- src/separators.js
+++ src/separators.js
@@ -1,9 +1,15 @@
 'use strict';
 
 function thousands_separators(num) {
-  if(num === 100) {
-    return '100';
+  var numarr = num.toString().split('');
+  var resultarr = [];
+  for(var i=numarr.length-1; i>=0; i--) {
+    resultarr.push(numarr[i]);
+    if(i%3===0) {
+      resultarr.push(',');
+    }
   }
+  return resultarr.join('');
 }
 
 module.exports = thousands_separators;

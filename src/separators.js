eaa65fcc6b01e420ec2fe24a2ec6a3ed2c8fca13
linwenjun <linwenjun.com>
2016-03-23 21:53:57 +0800

    fifth commit

--- src/separators.js
+++ src/separators.js
@@ -3,9 +3,9 @@
 function thousands_separators(num) {
   var numarr = num.toString().split('').reverse();
   var resultarr = [];
-  for(var i=0; i<=numarr; i++) {
-    resultarr.push(numarr[i]);
-    if(i%3===0) {
+  for(var i=0; i<numarr.length; i++) {
+    resultarr.unshift(numarr[i]);
+    if(i%3===2 && numarr.length > i+1) {
       resultarr.unshift(',');
     }
   }
